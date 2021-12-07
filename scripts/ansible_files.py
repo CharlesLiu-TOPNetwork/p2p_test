d1e0581caaa143fe508f4415ea7eb02332c6cbae
Charles.Liu <charles@localhost.localdomain>
2021-12-07 17:01:05 +0800

    update tar ansible file

--- scripts/ansible_files.py
+++ scripts/ansible_files.py
@@ -33,12 +33,23 @@ if __name__ == "__main__":
     print(cmd)
     os.system(cmd)
 
-    cmd = "ansible -i {0} all -f 6 -m copy -a \"src=./downloads/ dest=/tmp/p2p_test/ mode=0755 \"".format(
-        host_file)
-    print(cmd)
+    cmd = "rm -rf p2p_test_files.tar.gz"
+    os.system(cmd)
+    cmd = "tar -zcvf p2p_test_files.tar.gz ./downloads/* ./config"
     os.system(cmd)
 
-    cmd = "ansible -i {0} all -f 6 -m copy -a \"src=./config/ dest=/tmp/p2p_test/config/ \"".format(
-        host_file)
-    print(cmd)
+    cmd = "ansible -i {0} all -f 6 -m copy -a \"src=./p2p_test_files.tar.gz dest=/tmp/p2p_test/ \"".format(host_file)
     os.system(cmd)
+
+    cmd = "ansible -i {0} all -m shell -a \"cd /tmp/p2p_test && tar zxvf p2p_test_files.tar.gz && mv ./downloads/* ./ \" ".format(host_file)
+    os.system(cmd)
+
+    # cmd = "ansible -i {0} all -f 6 -m copy -a \"src=./downloads/ dest=/tmp/p2p_test/ mode=0755 \"".format(
+    #     host_file)
+    # print(cmd)
+    # os.system(cmd)
+
+    # cmd = "ansible -i {0} all -f 6 -m copy -a \"src=./config/ dest=/tmp/p2p_test/config/ \"".format(
+    #     host_file)
+    # print(cmd)
+    # os.system(cmd)
